ba5aad6e9a53937cba894d1a9111e7c2c18435d8
Yhakkk <yhakblanco@gmail.com>
2024-05-02 19:10:07 -0300

    creacion servidores

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor de suma corriendo en http://localhost:${PORT}`);
+  });
+
+
---
+++ microservices5.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT6 = process.env.PORT;
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor de suma corriendo en http://localhost:${PORT6}`);
+  });
+
+
