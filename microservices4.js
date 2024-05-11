f9982a2822ebc7b69b95f3cafb0c5db5c102a777
OctavioCalio <111621965+OctavioCalio@users.noreply.github.com>
2024-05-11 12:10:08 -0300

    Update microservices4.js
    Microservicio para deshabilitar usuarios

--- microservices4.js
+++ microservices4.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config();
-
+const { Pool } = require('pg');
+const cors = require("cors")
 const app = express();
-const PORT5 = process.env.PORT5;
+const PORT5 = process.env.PORT5; 
 
+app.use(express.json());
+app.use(cors());
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
 
+app.patch('/deshabilitar_usuario/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const client = await pool.connect();
+        const result = await client.query(
+            'UPDATE usuario SET habilitado = 0 WHERE id = $1 RETURNING *',
+            [userId]
+        );
+        client.release();
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.error('Error al ejecutar la query', err);
+        res.status(500).json({ error: 'Error al deshabilitar el usuario' });
+    }
+});
 
 app.listen(PORT5, () => {
-    console.log(`Servidor de suma corriendo en http://localhost:${PORT5}`);
-  });
+    console.log(`Servidor de deshabilitar usuario corriendo en http://localhost:${PORT5}`);
+});
 
 
